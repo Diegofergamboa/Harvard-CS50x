8cc05ab63f47b95174be0ce4b00476fb72c29b90
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-03-27 13:14:04 -0500

    Changes on review strings

--- testing/test_calculator2.py
+++ testing/test_calculator2.py
@@ -1,4 +1,5 @@
 from calculator import square
+import pytest
 
 def test_square():
     assert square(2) == 4
@@ -11,3 +12,7 @@ def test_negative():
 
 def test_zero():
     assert square(0) == 0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square('cat')
