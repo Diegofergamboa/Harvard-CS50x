86b93977c1adfeeabf83b94206e91a98efcd6090
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-03-27 09:26:40 -0500

    Using testing

--- testing/test_calculator2.py
+++ testing/test_calculator2.py
@@ -2,6 +2,12 @@ from calculator import square
 
 def test_square():
     assert square(2) == 4
-    assert square(3) == 9
+    assert square(7) == 49
+    
+
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+def test_zero():
+    assert square(0) == 0
