95336d1cc7390dfa205484beb034e54da571da99
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-03-26 19:46:39 -0500

    Working on manual testing

---
+++ testing/calculator.py
@@ -0,0 +1,12 @@
+def main():
+    x = int(input('What´s x? '))
+    result = square(x)
+    print(f'x squared is {result}')
+
+
+def square(n):
+    return n + n
+
+if __name__ == "__main__":
+    main()
+
