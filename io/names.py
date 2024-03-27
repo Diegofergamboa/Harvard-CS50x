c0e2626636c8870bcec90a34c73b989ada956772
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-03-27 16:01:36 -0500

    Files updating completed

---
+++ io/names.py
@@ -0,0 +1,26 @@
+# To create and update
+
+# name = input('what is your name? ')
+
+# with open("names.txt", "a") as file:
+#     file.write(f"{name}\n")
+#     file.close()
+
+
+# # To rewrite
+# with open("names.txt", "r") as file:
+#     for line in file:
+#         print("Hello, ", line.rstrip())
+
+
+
+# # To order
+# names = []
+
+# with open("names.txt") as file:
+#     for line in file:
+#         names.append(line.rstrip())
+
+
+# for name in sorted(names):
+#     print(f"hello, {name}")
